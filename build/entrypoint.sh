2ff3ecfc4ff9db0f527e88e9c1669cae466686e0
Mateusz Żółtak <zozlak@zozlak.org>
2024-10-14 22:05:39 +0200

    build/entrypoint.sh: another try

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,4 +3,4 @@ echo "$PGPASS" > /var/www/.pgpass &&\
     chmod 600 /var/www/.pgpass &&\
     chown www-data:www-data /var/www/.pgpass
 
-docker-php-entrypoint "$@"
+docker-php-entrypoint apache2-foreground
